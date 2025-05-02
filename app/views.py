9e30673b87f5b4296235ff60b0cc72a6d9a30aad
PrathyushaNandigam <nandigamprathyusha23@gmail.com>
2025-05-02 19:19:44 -0400

    Display uploaded files on the homepage and Keyword search added

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request,redirect, url_for, flash
 import os
 
 main = Blueprint('main', __name__)
@@ -7,7 +7,8 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', files=files)
 
 @main.route('/upload', methods=['POST'])
 def upload_files():
@@ -24,4 +25,25 @@ def upload_files():
     if cover_letter:
         cover_letter.save(os.path.join(UPLOAD_FOLDER, cover_letter.filename))
 
-    return render_template('index.html', message="Files uploaded successfully.")
+    return redirect(url_for('main.index'))
+import json
+
+@main.route('/save_filters', methods=['POST'])
+def save_filters():
+    title = request.form.get('title')
+    location = request.form.get('location')
+    keywords = request.form.get('keywords')
+
+    filters = {
+        "title": title,
+        "location": location,
+        "keywords": [kw.strip() for kw in keywords.split(',')] if keywords else []
+    }
+
+    # Save to a file (filters.json)
+    with open('filters.json', 'w') as f:
+        json.dump(filters, f, indent=4)
+
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', files=files, filter_message="Filters saved successfully.")
+
